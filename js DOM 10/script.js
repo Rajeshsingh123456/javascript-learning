0de55a70cd61c8a3ba5dd8172e0548a08a2b4f26
Rajesh <rajnegi1205@gmail.com>
2026-03-07 20:03:07 +0530

    added DOM manipulation

--- js DOM 10/script.js
+++ js DOM 10/script.js
@@ -48,5 +48,44 @@ let query=document.querySelectorAll(".heading");
 console.dir(query); // sare jitne bhi tag me heading di hai class saaare aa jayegneg 
 
 // by id 
-let id=document.querySelector("#heading-id");//id unique hoti hai isliye ek hi select karenge 
-console.dir(id);
+let de=document.querySelector("#heading-id");//id unique hoti hai isliye ek hi select karenge 
+console.dir(de);
+
+// dom manipulation Properties 
+//1 tag name = return karega kisi bhi element  ke node ka tag name .ex
+let first=document.querySelector(".click");
+console.dir(first);// Yeh element ko object form me show karta hai.
+//Matlab uske properties aur methods dekh sakte ho..
+console.log(first.tagName)// .tagname likhna padta hai tag name print ke liye isme humne tag ka name print kara diya konsa tag hai ye karke 
+
+// inner text = ye return karta hai node ke all element ka content child element (node)ka bhi content return karta hai change bhi kar sakte hai.
+//ex
+let div =document.querySelector("div");//select karega div elememnt ko
+console.dir(div);// object print kar dega us div me jo jo hoga pura object
+console.log(div.innerText);//ye print karayega div ke under jitna content hoga sab hi chil node ka bhi yahi div 
+// me ul li child nod ehai unka bhi content print hoga 
+//for change  ex=console.log(div.innerText="abc") = sare content ko change kar degi bs abc aayega bs
+
+
+//inner html =isme hume sare ke sare tags or content both aayenge inner text me bs text aata hai 
+// isme both tag or text both child node ka bhi  chnage bhi kar sakte hai ex.
+let inner=document.querySelector(".exinnerhtml");
+console.dir(inner); // isme object aa jayega 
+console.log(inner.innerHTML);// ye print kara dega 
+ // for change ex= 
+ let chng=document.querySelector("#chnge");
+ console.log(chnge.innerText="new heading");// heading 1 change ho jayegi new heading me innertext se 
+ console.log(chnge.innerHTML="<i>heading</i>") // ab new heading heading me aa jayegi italic me.
+
+ //4. text content= ye inner text ki tarah hi work kartahai but ye hidden eleemnt ke liye bhi kaam karta hai 
+// ex agar hum kisi elemet ko hide kar denge toh ye usme bhi kaam karega 
+//visibility css me hidden kar di ye heading nhi dikhegi toh ye js me text content lgaiye ye dikha dega
+let textcon=document.querySelector("#content");
+console.log(textcon.textContent)// dikh jayega vaha par ye 
+
+
+//attribute ka mtlb hai element ki undar  extra inforamtion (img me src link me src vgra ) 
+//JavaScript ki help se hum in attributes ko get, set, remove ya check kar sakte hain. ex
+let att=document.querySelector(".attribute");// attribute class hai usko select kar diya 
+console.log(att); // print kara diya pura div ko  ab  uske under class wali attribute haii uski value chahiye hume acces karni hai toh
+let value=att.getAttribute("class")// isme hume fun ko varible me store karke getattribute se class wali attribute ko select kar diya access kar diya ab iski value aa jayegi 
